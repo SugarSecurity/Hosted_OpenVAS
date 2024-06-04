7596bac266c638845bb677be19700376d9258e70
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-06-04 04:00:11 -0500

    fix: revert user_data
    Not executing as expected. Test with OG docstring

--- __main__.py
+++ __main__.py
@@ -4,15 +4,36 @@ import boto3
 import pulumi_aws as aws
 from pulumi import export
 from decouple import config
-from pathlib import Path
+# from pathlib import Path
 
 # Define EC2 instance details
 # cf. https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#AMICatalog:
 instance_type = config("INSTANCE_TYPE", default="t2.micro")
 ami = config("AMI", default="ami-00beae93a2d981137")
 
+# TODO: debug the user_data script
 # Define the User Data script
-user_data = Path("user_data.sh").read_text()
+# user_data = Path("user_data.sh").read_text()
+user_data = """#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install docker -y
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo yum install -y ca-certificates curl gnupg
+curl -f -O https://greenbone.github.io/docs/latest/_static/setup-and-start-greenbone-community-edition.sh
+chmod u+x setup-and-start-greenbone-community-edition.sh
+./setup-and-start-greenbone-community-edition.sh
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+export DOWNLOAD_DIR=$HOME/greenbone-community-container
+mkdir -p $DOWNLOAD_DIR
+cd $DOWNLOAD_DIR
+curl -f -L https://greenbone.github.io/docs/latest/_static/docker-compose-22.4.yml -o docker-compose.yml
+sudo docker-compose -f docker-compose.yml -p greenbone-community-edition pull
+sudo docker-compose -f docker-compose.yml -p greenbone-community-edition up -d
+"""
 
 # Create a boto3 session
 session = boto3.Session(
